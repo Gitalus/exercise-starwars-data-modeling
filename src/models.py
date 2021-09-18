1df2118980f4d6058ee0687853ed72a48e4d4d2b
Gitalus <italog.penaloza@gmail.com>
2021-09-18 22:22:38 +0000

    create Starship and Pilots model

--- src/models.py
+++ src/models.py
@@ -19,7 +19,7 @@ class Character(Base):
     birth_year = Column(String(20))
     gender = Column(String(20))
     homeworld = Column(Integer(), ForeignKey('planets.planet_id'))
-    starships = Column(Integer(), ForeignKey('starships.starship_id'))
+    starships = Column(Integer(), ForeignKey('pilots.starship_id'))
 
 
 class Planet(Base):
@@ -38,9 +38,34 @@ class Planet(Base):
 
 class Residents(Base):
     __tablename__ = 'residents'
-    # Here we define columns for the table planets.
+    # Here we define columns for the table residents.
     planet_id = Column(Integer(), ForeignKey('planets.planet_id'), primary_key=True)
     character = Column(Integer(), ForeignKey('characters.character_id'), primary_key=True)
+
+class Starship(Base):
+    __tablename__ = 'starships'
+    # Here we define columns for the table starships.
+    starship_id = Column(Integer(), primary_key=True)
+    name = Column(String(20), nullable=False)
+    model = Column(String(20))
+    manufacturer = Column(String(20))
+    cost_in_credits = Column(Integer())
+    length = Column(Float())
+    max_atmosphering_speed = Column(Float())
+    crew = Column(String(20))
+    passengers = Column(Integer())
+    cargo_capacity = Column(Float())
+    consumables = Column(String(20))
+    hyperdrive_rating = Column(Float())
+    MGLT = Column(Integer())
+    starship_class = Column(String(20))
+
+class Pilots(Base):
+    __tablename__ = 'pilots'
+    # Here we define columns for the table pilots.
+    starship_id = Column(Integer(), ForeignKey('starships.starship_id'), primary_key=True)
+    pilot = Column(Integer(), ForeignKey('characters.character_id'), primary_key=True)
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
