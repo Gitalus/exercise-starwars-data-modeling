c8b0ab96a3d091b9a0f273196dbaba67b40b3916
Gitalus <italog.penaloza@gmail.com>
2021-09-18 21:58:08 +0000

    create Character model

--- src/models.py
+++ src/models.py
@@ -1,33 +1,35 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Float
+from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
+
 from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Character(Base):
+    __tablename__ = 'characters'
+    # Here we define columns for the table characters
+    character_id = Column(Integer(), primary_key=True)
+    name = Column(String(50), nullable=False, unique=True)
+    heigth = Column(Float())
+    mass = Column(Float())
+    hair_color = Column(String(20))
+    skin_color = Column(String(20))
+    eye_color = Column(String(20))
+    birth_year = Column(String(20))
+    gender = Column(String(20))
+    homeworld = Column(Integer(), ForeignKey('planet.planet_id'))
+
 
 class Address(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
-
-    def to_dict(self):
-        return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
